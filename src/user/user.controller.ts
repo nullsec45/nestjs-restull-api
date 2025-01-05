c32f28b0efc6b42d1481501a6181abb7b3966dd2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-06 00:43:27 +0700

    feat/user : new user controller.

---
+++ src/user/user.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post,Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserRequest, UserResponse } from '../model/user.model';
+import { WebResponse } from '../model/web.model';
+
+@Controller('/api/users')
+export class UserController {
+    constructor(private userService:UserService){
+
+    }
+
+    @Post()
+    async register(
+        @Body() request:RegisterUserRequest
+    ):Promise<WebResponse<UserResponse>>{
+        const result=await this.userService.register(request);
+        console.log(result);
+
+        return {
+            data:result
+        }
+    }
+}
