64b0050dcfe7f96ce71a1e96a3275c1a39edfe81
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-06 00:46:06 +0700

    chroe(test) : new test service.

---
+++ test/test.service.ts
@@ -0,0 +1,27 @@
+import { Prisma } from "@prisma/client";
+import { PrismaService } from "../src/common/prisma.service";
+import { Injectable } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class TestService{
+    constructor(private prismaService:PrismaService){}
+
+    async deleteUser(){
+        await this.prismaService.user.deleteMany({
+            where:{
+                username:'test'
+            }
+        })
+    }
+
+    async createUser(){
+        await this.prismaService.user.create({
+            data:{
+                username:'test',
+                name:'test',
+                password:await bcrypt.hash('test12345678',10)
+            }
+        })
+    }
+}
