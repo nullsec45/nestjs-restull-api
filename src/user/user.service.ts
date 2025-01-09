3ee63327ef3f44a17921a6a977ae4b7811b95535
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-09 23:44:24 +0700

    feat/login : login and get user in service.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,25 +2,27 @@ import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { PrismaService } from '../common/prisma.service';
 import { ValidationService } from '../common/validation.service';
-import { RegisterUserRequest, UserResponse } from '../model/user.model';
+import { LoginUserRequest, RegisterUserRequest, UserResponse } from '../model/user.model';
 import {Logger} from 'winston';
 import { UserValidation } from './user.validation';
 import * as bcrypt from 'bcrypt';
+import {v4 as uuid} from 'uuid';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
     constructor(
         private validationService:ValidationService,
         @Inject(WINSTON_MODULE_PROVIDER) private logger:Logger,
-        private prismaServicve:PrismaService
+        private prismaService:PrismaService
     ){
 
     }
+
     async register(request:RegisterUserRequest):Promise<UserResponse>{
-        this.logger.info(`Register new user ${JSON.stringify(request)}`);
         const registerRequest=this.validationService.validate(UserValidation.REGISTER, request);
 
-        const totalUserWithSameUsername=await this.prismaServicve.user.count({
+        const totalUserWithSameUsername=await this.prismaService.user.count({
             where:{
                 username:registerRequest.username
             }
@@ -32,7 +34,7 @@ export class UserService {
 
         registerRequest.password = await bcrypt.hash(registerRequest.password,10);
 
-        const user=await this.prismaServicve.user.create({
+        const user=await this.prismaService.user.create({
             data:registerRequest
         });
         
@@ -41,5 +43,54 @@ export class UserService {
             username:user.username,
             name:user.name
         };
+    }
+
+    async login(request:LoginUserRequest):Promise<UserResponse>{
+        const loginRequest:LoginUserRequest=this.validationService.validate(
+            UserValidation.LOGIN,
+            request
+        );
+
+        let user=await this.prismaService.user.findUnique({
+            where:{
+                username:loginRequest.username
+            }
+        });
+
+        if(!user){
+            throw new HttpException('Username or password is invalid',401);
+        }
+
+        const isPasswordValid=await bcrypt.compare(
+            loginRequest.password,
+            user.password
+        );
+
+        if(!isPasswordValid){
+            throw new HttpException('Username or password is invalid',401);
+        }
+
+        user=await this.prismaService.user.update({
+            where:{
+                username:loginRequest.username
+            },
+            data:{
+                token:uuid()
+            }
+        });
+
+        return {
+            username:user.username,
+            name:user.name,
+            token:user.token
+        }
+    }
+
+
+    async get(user:User):Promise<UserResponse>{
+        return {
+            username:user.username,
+            name:user.name
+        }
     }
 }
