384d79338f406ac1d9ed9c83b66cf937b3f653b1
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-09 23:45:28 +0700

    feat/login : testing login and get user.

--- test/user.spec.ts
+++ test/user.spec.ts
@@ -24,8 +24,8 @@ describe('UserController', () => {
     testService=app.get(TestService);
   });
 
-  describe('POST /api/users',() => {
-    afterEach(async() => {
+  describe.skip('POST /api/users',() => {
+    beforeEach(async() => {
       await testService.deleteUser();
     });
 
@@ -60,7 +60,7 @@ describe('UserController', () => {
         expect(response.body.data.name).toBe('test');
     });
 
-    it('should be able to register', async() => {
+    it('should be able rejected if username already exists', async() => {
         await testService.createUser();
         const response= await request(app.getHttpServer())
                               .post('/api/users')
@@ -74,6 +74,73 @@ describe('UserController', () => {
 
         expect(response.status).toBe(400);
         expect(response.body.errors).toBeDefined();
+    });
+  });
+
+  describe.skip('POST /api/users/login',() => {
+    beforeEach(async() => {
+      await testService.deleteUser();
+      await testService.createUser();
+    });
+
+    it('should be rejected if request is invalid', async() => {
+        const response= await request(app.getHttpServer())
+                              .post('/api/users/login')
+                              .send({
+                                username:'',
+                                password:'',
+                              });
+
+        logger.info(response.body);
+                              
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toBeDefined();
+    });
+
+     it('should be able to login', async() => {
+        const response= await request(app.getHttpServer())
+                              .post('/api/users/login')
+                              .send({
+                                username:'test',
+                                password:'test12345678',
+                              });
+        
+        logger.info(response.body);          
+
+        expect(response.status).toBe(201);
+        expect(response.body.data.username).toBe('test');
+        expect(response.body.data.name).toBe('test');
+        expect(response.body.data.token).toBeDefined();
+    });
+  });
+
+  describe('GET /api/users/current',() => {
+    beforeEach(async() => {
+      await testService.deleteUser();
+      await testService.createUser();
+    });
+
+    it.skip('should be rejected if token is invalid', async() => {
+        const response= await request(app.getHttpServer())
+                              .get('/api/users/current')
+                              .set('Authorization','wrong');
+
+        logger.info(response.body);
+                              
+        expect(response.status).toBe(401);
+        expect(response.body.errors).toBeDefined();
+    });
+
+     it('should be able to get user', async() => {
+        const response= await request(app.getHttpServer())
+                              .get('/api/users/current')
+                              .set('Authorization','test');
+
+        // logger.info(response.body);          
+
+        expect(response.status).toBe(200);
+        expect(response.body.data.username).toBe('test');
+        expect(response.body.data.name).toBe('test');
     });
   });
 });
