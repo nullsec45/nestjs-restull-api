55a39dfa49e455d09da5299ce325810d51d758cc
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-09 23:44:08 +0700

    feat/login : login and get user in controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Post,Body } from '@nestjs/common';
+import { Controller, Post,Body, Get,Inject } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserRequest, UserResponse } from '../model/user.model';
 import { WebResponse } from '../model/web.model';
+import { Auth } from '../common/auth.decorator';
+import { User } from '@prisma/client';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import {Logger} from 'winston';
+
 
 @Controller('/api/users')
 export class UserController {
-    constructor(private userService:UserService){
+    constructor(private userService:UserService,    @Inject(WINSTON_MODULE_PROVIDER) private logger:Logger,){
 
     }
 
@@ -14,10 +19,32 @@ export class UserController {
         @Body() request:RegisterUserRequest
     ):Promise<WebResponse<UserResponse>>{
         const result=await this.userService.register(request);
-        console.log(result);
 
         return {
             data:result
         }
     }
+
+    @Post('/login')
+    async login(
+        @Body() request:RegisterUserRequest
+    ):Promise<WebResponse<UserResponse>>{
+        const result=await this.userService.login(request);
+
+        return {
+            data:result
+        }
+    }
+
+    @Get('/current')
+    async get(
+        @Auth() user:User
+    ):Promise<WebResponse<UserResponse>>{
+        const result=await this.userService.get(user);
+
+        return {
+            data:result
+        }
+    }
+
 }
